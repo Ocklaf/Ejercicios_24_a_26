48cfecee2035f4db57eef4e9d1f5fd73c020b2a7
Ocklaf <ocklaf@gmail.com>
2021-06-07 13:32:27 +0200

    Final_jonmircha_yo_no_he_resuelto_ninguno

--- script-jonmircha.js
+++ script-jonmircha.js
@@ -4,6 +4,32 @@ numeros ordenados en forma ascendente y el segundo de forma
 descendiente, pe. miFuncion([7, 5,7,8,6]) 
 devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.*/
 
+/* const ordenarArray = (arr = undefined) => { 
+
+    if(arr === undefined) return console.warn("No has introducido un Array")
+
+    if(!(arr instanceof Array)) return console.error("No es un Array lo que has introducido")
+
+    //comprobamos si hay elementos, si no los hay, length será 0, por lo tanto, estará vacío
+    if(arr.length === 0) return console.error("El Array está vacío")
+
+    //necesitamos ver que todos los elementos sean números
+    for (let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor de ${num} no es un número`)
+    }
+
+    return console.info({
+        arr,
+        asc: arr.map(el => el).sort(),
+        desc: arr.map(el => el).sort().reverse()
+    })
+}
+
+ordenarArray()
+ordenarArray("hola")
+ordenarArray({})
+ordenarArray([])
+ordenarArray([7, 5,7,8,6]) */
 
 
 
@@ -13,9 +39,67 @@ elimine los duplicados,
 pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true])
  devolverá ["x", 10, 2, "10", true].*/
 
+/* const quitarDuplicados = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No has introducido un Array")
 
+    if(!(arr instanceof Array)) return console.error("No es un Array lo que has introducido")
+
+    //comprobamos si hay elementos, si no los hay, length será 0, por lo tanto, estará vacío
+    if(arr.length === 0) return console.error("El Array está vacío")
+
+    //comprobamos que tenta mínimo 2 elementos, de lo contrario no tiene sentido hacer una comparación
+    if(arr.length === 1) return console.error("El Array está vacío")
+
+    //    return console.info({ //como se haría de ENMAScript 5 hacia atrás
+    //    original: arr,
+    //    //Sirve para explicar filter() e indexOf()
+    //    sinDuplicados: arr.filter((value, index, self) => self.indexOf(value) === index)
+    // }) 
+
+    return console.info({ //como se haría de ENMAScript 6 en adelante
+        original: arr,
+        //nuevo tipo de dato ENMAScript 6, NO permite elementos/datos duplicados
+        sinDuplicados: [...new Set(arr)]
+    }) 
+}
+
+quitarDuplicados()
+quitarDuplicados({})
+quitarDuplicados([])
+quitarDuplicados([2])
+quitarDuplicados(["x", 10, "x", 2, "10", 10, true, true]) */
 
 
 /*26) Programa una función que dado un arreglo de 
 números obtenga el promedio, 
 pe. promedio([9,8,7,6,5,4,3,2,1,0] devolverá 4.5. */
+
+const promedio = (arr = undefined) => { 
+
+    if(arr === undefined) return console.warn("No has introducido un Array")
+
+    if(!(arr instanceof Array)) return console.error("No es un Array lo que has introducido")
+
+    //comprobamos si hay elementos, si no los hay, length será 0, por lo tanto, estará vacío
+    if(arr.length === 0) return console.error("El Array está vacío")
+
+    //necesitamos ver que todos los elementos sean números
+    for (let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor de ${num} no es un número`)
+    }
+
+    return console.info(
+        arr.reduce((total, num, index, arr) =>{
+            total += num
+            if(index === arr.length - 1){
+                return `El promedio de ${arr.join(" + ")} es ${total / arr.length}`
+            }else return total
+        })
+    )
+}
+
+promedio()
+promedio({})
+promedio([])
+promedio(2, true)
+promedio([1,2,3,4,5,6,7,8,9,0])
--- script.js
+++ script.js
@@ -4,8 +4,36 @@ numeros ordenados en forma ascendente y el segundo de forma
 descendiente, pe. miFuncion([7, 5,7,8,6]) 
 devolverá { asc: [5,6,7,7,8], desc: [8,7,7,6,5] }.*/
 
+/* const arrayParesEImpares = (arr = undefined) =>{
+    if(arr === undefined) return console.warn("No has introducido un Array")
 
+    if(!(arr instanceof Array)) return console.error("No es un Array lo que has introducido")
 
+    //comprobamos si hay elementos, si no los hay, length será 0, por lo tanto, estará vacío
+    if(arr.length === 0) return console.error("El Array está vacío")
+
+    //necesitamos ver que todos los elementos sean números
+    for (let num of arr) {
+        if(typeof num !== "number") return console.error(`El valor de ${num} no es un número`)
+    }
+
+    let miObjeto = {
+            ascendente: [],
+            descendente: []
+         }
+
+     miObjeto.ascendente.push(arr.sort())
+     //arr=arr.slice("").toString()
+     console.info(arr)
+     arr=arr.reverse()
+     console.info(arr)
+
+     miObjeto.descendente.push(arr)
+
+    console.info(miObjeto)
+}
+
+arrayParesEImpares([7,5,7,8,6]) */
 
 
 /*25) Programa una función que dado un arreglo de elementos,
@@ -13,7 +41,36 @@ elimine los duplicados,
 pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true])
  devolverá ["x", 10, 2, "10", true].*/
 
+ const arrayIncluye = (arr = undefined) =>{
+    if(arr === undefined) return console.warn("No has introducido un Array")
 
+    if(!(arr instanceof Array)) return console.error("No es un Array lo que has introducido")
+
+    //comprobamos si hay elementos, si no los hay, length será 0, por lo tanto, estará vacío
+    if(arr.length === 0) return console.error("El Array está vacío")
+ 
+    let repetido = [],
+        i = 0
+
+    while(arr.includes(arr[i]) === -1){
+        repetido.push(arr[i])
+        i++
+    }
+    console.log(repetido)
+}
+
+/* var indices = [];
+var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+var element = 'a';
+var idx = array.indexOf(element);
+while (idx != -1) {
+  indices.push(idx);
+  idx = array.indexOf(element, idx + 1);
+}
+console.log(indices); */
+// [0, 2, 4]
+
+arrayIncluye(["x", 10, "x", 2, "10", 10, true, true])
 
 
 /*26) Programa una función que dado un arreglo de 
